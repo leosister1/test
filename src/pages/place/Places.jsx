7b26dfa15c7c74d39fe881450bd322c528a18841
leosister1 <114340928+leosister1@users.noreply.github.com>
2023-04-08 18:39:37 +0900

    .
    .

--- src/pages/place/Places.jsx
+++ src/pages/place/Places.jsx
@@ -1,35 +1,68 @@
 import { useState, useEffect } from "react";
 
-import axios from "axios";
 import PlacesMap from "../../components/PlacesMap";
 import Place from "../../components/Place";
 
 const dummyData = [
   {
-    
+    _id: 0,
+    placeName: "한국타이어",
+    desc: "정품 품질 보장", 
   },
+  {
+    _id: 1,
+    placeName: "현대모비스",
+    desc: "합리적 가격", 
+  },
+  {
+    _id: 2,
+    placeName: "티스테이션",
+    desc: "안전과 최적화된 주행을 우선", 
+  },
+  {
+    _id: 3,
+    placeName: "오토팜",
+    desc: "엔진 오일 교환", 
+  },
+  {
+    _id: 4,
+    placeName: "현대블루핸즈",
+    desc: "10년째 운영중", 
+  },
+  {
+    _id: 5,
+    placeName: "태성카센터",
+    desc: "10년째 운영중", 
+  }
 ];
 
 const Plcaes = () => {
-  const [data, setData] = useState(dummyData);
+  const [data] = useState(dummyData);
 
   useEffect(() => {
-    axios
-      .get("/api/place")
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="container w-[85%] xl:max-w-10xl grid grid-cols-1 lg:grid-cols-3 absolute top-20 bottom-0 left-20.0 right-10">
+    <div>
+    <div className="container w-[85%] xl:max-w-10xl grid grid-cols-1 lg:grid-cols-3 flex">
       <div className="col-span-2 min-h-[30vh]">
         <PlacesMap height="85%" data={data} />
       </div>
-      <div className="places col-span-1 mt-10 mb-5 mx-auto lg:mr-5 overflow-y-scroll">
+      <div className="places col-span-1 mt-2 mb-12 lg:mr-1 overflow-y-scroll">
         {data.map((item) => {
           return <Place key={item._id} {...item} />;
         })}
-      </div>
+    </div>
+    </div>
+        <div className="flex">
+          <div className="grid h-20 flex-grow card bg-base-200 rounded-box place-items-center">AD</div>
+          <div className="divider divider-horizontal"></div>
+          <div className="grid h-20 flex-grow card bg-base-200 rounded-box place-items-center">AD</div>
+          <div className="divider divider-horizontal"></div>
+          <div className="grid h-20 flex-grow card bg-base-200 rounded-box place-items-center">news</div>
+          <div className="divider divider-horizontal"></div>
+          <div className="grid h-20 flex-grow card bg-base-200 rounded-box place-items-center">news</div>
+        </div>    
     </div>
   );
 };
