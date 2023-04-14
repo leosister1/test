1bcbe39977976c3879a0e378487d9a0543a1462f
leosister1 <114340928+leosister1@users.noreply.github.com>
2023-04-14 22:47:41 +0900

    Update Place.jsx

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -8,8 +8,18 @@ const Place = ({ _id, placeName, desc, image }) => {
       className="place-container grid grid-cols-5 h-1/5 rounded-lg py-3 hover:cursor-pointer"
       onClick={() => navigate(`/place/${_id}`)}
     >
-      
+    <div className="image col-span-1 rounded-lg bg-base-200 overflow-hidden">
+      <img
+        className="object-cover w-full h-full"
+        src={image}
+        alt={`${placeName} 업체사진`}
+      />
     </div>
+    <div className="desc col-span-3 rounded-lg ml-3">
+      <p className="place-name font-bold mb-1">{placeName}</p>
+      <p className="place-desc text-xs text-base-300 truncate">{desc}</p>
+    </div>
+  </div>
   );
 };
 
