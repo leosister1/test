d9540a1f049955998d2972e62878d7caeb570f7d
leosister1 <114340928+leosister1@users.noreply.github.com>
2023-04-15 16:54:45 +0900

    Update MyPage.jsx

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -9,37 +9,21 @@ export default function MyPage() {
           <div className="chat-bubble bg-base-200 text-black w-60 text-center ml-8 mt-10">반가워요🙂 유진 님</div>
         </div>
       </div>
-
-        <div className="navbar place-content-center bg-base-content">
-          <div className="flex justify-end flex px-2">
-            <div className="flex items-stretch">
-            <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn text-white px-20">회원정보</label>
-                <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                  <li><a>Item 1</a></li>
-                </ul>
-              </div>
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn text-white px-20">예약목록</label>
-                <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                  <li><a>Item 1</a></li>
-                </ul>
-              </div>
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn text-white px-20">내 차 목록</label>
-                <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                  <li><a>Item 1</a></li> 
-                </ul>
-              </div>
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn text-white px-20">즐겨찾기</label>
-                <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                  <li><a>Item 1</a></li> 
-                </ul>
-              </div>
-            </div>
-          </div>
+      <div className="divider"></div> 
+      <div className="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content flex flex-col items-center justify-center">
+    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">dhjslafr</label>
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div>
+        
         </div>
-      </div>
   )
 };
